b0bc5f2b6e587faa2b0b6c0c055760c38a93ab9c
rmathew-uni <rmathew@uni-osnabrueck.de>
2024-10-31 13:09:59 +0100

    Updated Docs notebook

--- src/setup.py
+++ src/setup.py
@@ -1,15 +1,26 @@
-# from setuptools import setup, find_packages
+from setuptools import setup, find_packages
 
-# setup(
-#     name="excel-column-sum",
-#     version="0.1",
-#     packages=find_packages(),
-#     install_requires=[
-#         "pandas",
-#         "openpyxl",  # Required for reading Excel files
-#     ],
-#     author="Your Name",
-#     author_email="your_email@example.com",
-#     description="A package that reads an Excel file and sums all the columns",
-#     url="https://github.com/your-username/excel-column-sum",
-# )
+setup(
+    name="excel-column-sum",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+    scipy,
+    statistics,
+    numpy,
+    pandas,
+    matplotlib,
+    scikit-learn,
+    gpboost,
+    xgboost,
+    shap,
+    merf,
+    openpyxl,
+    xlsxwriter,
+    featurewiz",
+],
+    author="Your Name",
+    author_email="your_email@example.com",
+    description="A package that reads an Excel file and sums all the columns",
+    url="https://github.com/your-username/excel-column-sum",
+)
